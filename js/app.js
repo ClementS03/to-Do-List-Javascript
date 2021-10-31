dc1ee754ee311d0bf7c18cfc0376cb0603c76c82
ClementS03 <clement.seguin63@gmail.com>
2021-10-31 14:03:43 +0100

    Ajout d'une Tâche TODO: mettre à jour le compteur

--- js/app.js
+++ js/app.js
@@ -4,10 +4,9 @@
 var app = {
   init: function () {
 
+    // Init Counter, Container and Count for use in our app
     app.container = document.getElementById('todo');
-
     app.counter = document.createElement('p');
-
     app.count = 0;
 
     app.createForm();
@@ -25,6 +24,8 @@ var app = {
     input.className = 'input_addTask';
     form.appendChild(input);
 
+    form.addEventListener('submit', app.handleSubmit);
+
     app.container.appendChild(form);
   },
 
@@ -39,27 +40,9 @@ var app = {
   },
 
   createListTasks: function () {
-    const ul = document.createElement('ul');
+    app.ul = document.createElement('ul');
 
-    for (let i = 0; i < 2; i++) {
-      const taskContainer = document.createElement('li');
-      taskContainer.className = 'taskContainer';
-
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.className = 'taskCheckbox';
-
-      const label = document.createElement('label');
-      label.textContent = 'Faire une action';
-      label.className = 'taskLabel';
-
-      taskContainer.appendChild(checkbox);
-      taskContainer.appendChild(label);
-
-      ul.appendChild(taskContainer);
-    }
-
-    app.container.appendChild(ul);
+    app.container.appendChild(app.ul);
   },
 
   setCounterValue: function () {
@@ -68,8 +51,45 @@ var app = {
     } else {
       app.counter.textContent = `${app.count} tâche en cours`
     }
-  }
+  },
 
+  generateTask: function (taskTitle) {
+    // Create container for tasks
+    const taskContainer = document.createElement('li');
+    taskContainer.className = 'taskContainer';
+
+    const id = 'checkbox-' + taskTitle;
+
+    // Create Checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'taskCheckbox';
+    checkbox.id = id;
+
+    // Create Label
+    const label = document.createElement('label');
+    label.textContent = taskTitle;
+    label.className = 'taskLabel';
+    label.setAttribute('for', id);
+
+    taskContainer.appendChild(checkbox);
+    taskContainer.appendChild(label);
+
+    app.ul.appendChild(taskContainer);
+  },
+
+  handleSubmit: function (event) {
+    event.preventDefault();
+
+    const inputValue = event.target.children[0].value;
+
+    if (inputValue.trim()) {
+      app.generateTask(inputValue);
+      event.target.children[0].value = "";
+    } else {
+      console.log('Attention le champ est vide !!');
+    }
+  }
 };
 
 
